1f84644f285b360e956e84bab764b119f3e31183
Masayoshi0609 <masayoshi9876@gmail.com>
2021-07-28 00:20:02 +0000

    [Update]5-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
     redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = @user.books.all
+      render :index
+    end
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
     redirect_to user_path(@user.id), flash: { updated: 'You have updated user successfully.', class: "text-success" }
+    else
+      render :edit
+    end
   end
 
   private
