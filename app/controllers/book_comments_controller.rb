4c7e2d468813902d5c743bf8e4b55e5342bfe529
Masayoshi0609 <masayoshi9876@gmail.com>
2021-08-05 10:22:24 +0000

    [Finish]6-3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,25 @@
 class BookCommentsController < ApplicationController
-	
+
 	def create
+	 @book_detail = Book.find(params[:book_id])
+	 comment = BookComment.new(book_comment_params)
+	 comment.user_id = current_user.id
+	 comment.book_id = @book_detail.id
+	 comment.save
+	 redirect_to book_path(@book_detail)
 	end
-	
+
 	def destroy
+	 BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+	 redirect_to book_path(params[:book_id])
 	end
-	
+
+
+	private
+
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
+
 end
+
