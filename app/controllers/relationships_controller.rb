ea834987d903245bf0a1764890242bb3c76f5599
Masayoshi0609 <masayoshi9876@gmail.com>
2021-08-07 12:59:45 +0000

    [Finish]6-4

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,29 @@
+class RelationshipsController < ApplicationController
+
+
+#フォロー作成、保存、削除
+
+ def create
+ 	current_user.follow(params[:user_id])
+ 	redirect_to request.referer
+ end
+
+ def destroy
+ 	current_user.unfollow(params[:user_id])
+ 	redirect_to request.referer
+ end
+
+
+#フォロー・フォロワーの一覧表示
+
+ def followings
+ user = User.find(params[:user_id])
+ @users = user.followings
+ end
+
+ def followers
+ user = User.find(params[:user_id])
+ @users = user.followers
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update, :new, :index]
+  resources :users, only: [:show, :edit, :update, :new, :index] do
+   resource :relationships, only: [:create, :destroy]
+   get "followings" => "relationships#followings", as: "followings"
+   get "followers" => "relationships#followers", as: "followers"
+  
+  end      
  
   post "users/:id/edit" => "users#edit"
   post "users/:id" => "users#create"
